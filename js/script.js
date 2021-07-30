6cfb2d0d3f48450b834b89164d03a858cd78cfda
unknown <66135644+gladsbc0ding@users.noreply.github.com>
2021-07-30 12:49:11 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// new variable - appears when guest list is full
+const assignButton = document.querySelector(".assign");
+// new variable - targets list that populates guest's name and assigned dish
+const assignedItems = document.querySelector(".assigned-items");
+
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  //console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+    updateGuestCount();
+  }
+  clearInput();
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  let guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+// select elements & loop through the array
+const assignItems = function () {
+  let potluckItems = [
+    "mac salad",
+    "poi",
+    "kalua pork sliders",
+    "huli huli chicken",
+    "lilikoi ice cream",
+    "mango cake",
+    "poke salad",
+    "brown rice",
+    "loco moco",
+    "fish tacos",
+    "lomi lomi salmon",
+    "garlic noodles"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+  for (var guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+// add event listener & update PotluckItems array
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
